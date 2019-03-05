429d26abd7b55a143eb31de3f60a7825764ec83a
Gianni Cafano <GianniCafano@Giannis-MacBook-Pro.local>
2019-03-05 21:28:19 +0000

    added in album art and defensive code and visual improvments

--- src/components/TrackDetail.vue
+++ src/components/TrackDetail.vue
@@ -1,10 +1,23 @@
 <template>
     <div class="container">
-        <h1>{{this.trackInfo.track_name}}</h1>
-        <p>
-            <img :src="this.trackLyrics.pixel_tracking_url">
-            {{this.trackLyrics.lyrics_body}}
-        </p>
+        <div class="row">
+            <div class="col-md-3">
+                <div v-if="albumInfo.album_coverart_100x100" class="cover-art" :style='{ backgroundImage: "url(" + albumInfo.album_coverart_100x100 + ")", }'>
+                </div>
+                <div v-else class="cover-art" style="background-image:url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/eb777e7a-7d3c-487e-865a-fc83920564a1/d7kpm65-437b2b46-06cd-4a86-9041-cc8c3737c6f0.jpg)">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h1>{{this.trackInfo.track_name}}</h1>
+                <h4>{{this.trackInfo.artist_name}}</h4>
+            </div>
+            <section class="col-md-12 mt-5">
+                <p class="lyrics">
+                    <img :src="this.trackLyrics.pixel_tracking_url">
+                    {{this.trackLyrics.lyrics_body}}
+                </p>
+            </section>
+        </div>
     </div>
 </template>
 
@@ -31,7 +44,7 @@ export default {
   },
     methods:{
         async getTrack(){
-            ApiService.get(`https://api.musixmatch.com/ws/1.1/track.get?apikey=${this.apiKey}&commontrack_id=${this.commontrack_id}`)
+            await ApiService.get(`https://api.musixmatch.com/ws/1.1/track.get?apikey=${this.apiKey}&commontrack_id=${this.commontrack_id}`)
             .then((response) =>{
                 this.trackInfo = response.data.message.body.track;
                 this.album_id = response.data.message.body.track.album_id;
@@ -44,7 +57,7 @@ export default {
         },
         
         async getLyrics(){
-            ApiService.get(`https://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=${this.apiKey}&commontrack_id=${this.commontrack_id}`)
+            await ApiService.get(`https://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=${this.apiKey}&commontrack_id=${this.commontrack_id}`)
             .then((response) =>{
                 this.trackLyrics = response.data.message.body.lyrics;
                 console.log('lyrics:', this.trackLyrics);  
@@ -55,9 +68,9 @@ export default {
         },
     
         async getAlbum(){
-            ApiService.get(`https://api.musixmatch.com/ws/1.1/album.get?apikey=${this.apiKey}&album_id=${this.album_id}`)
+            await ApiService.get(`https://api.musixmatch.com/ws/1.1/album.get?apikey=${this.apiKey}&album_id=${this.album_id}`)
             .then((response) =>{
-                this.albumInfo = response.data;
+                this.albumInfo = response.data.message.body.album;
                 console.log('album info', this.albumInfo);
                 console.log(`https://api.musixmatch.com/ws/1.1/album.get?apikey=${this.apiKey}&album_id=${this.album_id}`);
             })
